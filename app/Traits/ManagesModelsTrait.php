e441f0501808bb8b73c4c3a2e042948a68ed9ac4
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-07-16 14:41:40 +0200

    category

---
+++ app/Traits/ManagesModelsTrait.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Traits;
+
+
+
+trait ManagesModelsTrait {
+    public function destroyModel(string $modelClass, string $resourceClass, string $id){
+        $this->authorize('manage_users');
+        $modelInstance = $modelClass::find($id);
+        if (!$modelInstance) {
+            return response()->json([
+                'message' => class_basename($modelClass) . " not found."
+            ], 404);
+        }
+
+        $modelInstance->delete();
+        return response()->json([
+            'data' => new $resourceClass($modelInstance),
+            'message' => "Soft Delete " . class_basename($modelClass) . " By Id Successfully."
+        ]);
+    }
+
+    public function showDeletedModels(string $modelClass, string $resourceClass){
+        $this->authorize('manage_users');
+        $models = $modelClass::onlyTrashed()->get();
+        return response()->json([
+            'data' => $resourceClass::collection($models),
+            'message' => "Show Deleted " . class_basename($modelClass) . " Successfully."
+        ]);
+    }
+
+    public function restoreModel(string $modelClass, string $id){
+        $this->authorize('manage_users');
+        $modelInstance = $modelClass::withTrashed()->where('id', $id)->first();
+        if (!$modelInstance) {
+            return response()->json([
+                'message' => class_basename($modelClass) . " not found."
+            ], 404);
+        }
+
+        $modelInstance->restore();
+        return response()->json([
+            'message' => "Restore " . class_basename($modelClass) . " By Id Successfully."
+        ]);
+    }
+
+    public function forceDeleteModel(string $modelClass, string $id){
+        $this->authorize('manage_users');
+        $modelInstance = $modelClass::withTrashed()->where('id', $id)->first();
+        if (!$modelInstance) {
+            return response()->json([
+                'message' => class_basename($modelClass) . " not found."
+            ], 404);
+        }
+
+        $modelInstance->forceDelete();
+        return response()->json([
+            'message' => "Force Delete " . class_basename($modelClass) . " By Id Successfully."
+        ]);
+    }
+}
