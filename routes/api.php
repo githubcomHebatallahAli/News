bc45b49f54973a4781fd86cf38d12565f25c19dd
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-07-16 21:49:07 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\RoleController;
 
-Route::post('/create', [RoleController::class, 'create']) ;
+// Route::post('/create', [RoleController::class, 'create']) ;
 
 
 
