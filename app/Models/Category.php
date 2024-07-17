2fbe47b118a456d96a68aef5bbf78cf313cfae0d
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-07-17 15:52:46 +0200

    update category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,5 +11,6 @@ class Category extends Model
     use HasFactory , SoftDeletes ;
     protected $fillable = [
         'name',
+        'url'
     ];
 }
--- database/migrations/2024_07_16_105255_create_categories_table.php
+++ database/migrations/2024_07_16_105255_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('url');
+            $table->string('url')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
