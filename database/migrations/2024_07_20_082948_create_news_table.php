d23590a5455ad26389bec98b3bdd3ca66dcf133c
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-07-20 11:29:37 +0200

    news

---
+++ database/migrations/2024_07_20_082948_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('categoryName');
+            $table->string('title');
+            $table->string('writer');
+            $table->date('event_date')->nullable();
+            $table->string('img')->nullable();
+            $table->text('text1')->nullable();
+            $table->text('text2')->nullable();
+            $table->text('text3')->nullable();
+            $table->string('keyWords');
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
---
+++ routes/Apis/Admin/news.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\NewsController;
+
+Route::controller(NewsController::class)->prefix('/admin')->middleware('admin')->group(
+    function () {
+
+   Route::get('/showAll/news','showAll');
+   Route::post('/create/news', 'create');
+   Route::get('/edit/news/{id}','edit');
+   Route::post('/update/news/{id}', 'update');
+   Route::delete('/delete/news/{id}', 'destroy');
+   Route::get('/showDeleted/news', 'showDeleted');
+Route::get('/restore/news/{id}','restore');
+Route::delete('/forceDelete/news/{id}','forceDelete');
+   });
